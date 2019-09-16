13daeb928f9dbf6f95e9dbc45ee5e0c13b995b00
tinsmore <mrdhf1997@163.com>
2019-09-16 23:50:59 +0800

    to merge with other agents

--- agent/__init__.py
+++ agent/__init__.py
@@ -1,4 +1,6 @@
 from .base import BaseAgent
-from .rl_agent import RLAgent 
+from .rl_agent import RLAgent
+from .max_pressure_agent import MaxPressureAgent
+from .sotl_agent import SOTLAgent
 from .fixedtime_agent import Fixedtime_Agent
 from .qmix_agent import QMix_Agent
